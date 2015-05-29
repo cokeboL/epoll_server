2dc083b37cfb5b7c373cc2d3372cb3529ac0345f
cokeboL <165128167@qq.com>
2015-05-29 20:08:47 +0800

    -

--- server/src/list.h
+++ server/src/list.h
@@ -4,11 +4,13 @@
 #include "commen.h"
 
 typedef void (*ListNodeReleaseHandler)(void *);
+typedef void (*ListNodeTraverseHandler)(void *);
+
 
 typedef struct ListNode
 {
-	void *pre;
-	void *next;
+	ListNode *pre;
+	ListNode *next;
 	void *data;
 }ListNode;
 
@@ -16,29 +18,32 @@ typedef struct List
 {
 	ListNode *head;
 	ListNode *tail;
-	uint32_t count;
+	int count;
 	ListNodeReleaseHandler release_handler;
 }List;
 
 #define LIST_CREATE() \
 	(List*)Malloc(sizeof(List))
 
-#define LIST_INIT(list, release_handler) \
-	if(list){ \
-		list->head = list->tail = list->count = 0; \
-		list->release_handler = release_handler; \
+#define LIST_INIT(list, handler) \
+	if (list){ \
+		list->head = 0; \
+		list->tail = 0; \
+		list->count = 0; \
+		list->release_handler = (ListNodeReleaseHandler)handler; \
 	}
 
-#define LIST_PUSH(list, data) \
+#define LIST_PUSH(list, value) \
 	if(list->head){ \
-		ListNode *node = Malloc(sizeof(ListNode)); \
+	ListNode *node = (ListNode*)Malloc(sizeof(ListNode)); \
 		node->pre = list->tail; \
 		node->next = 0; \
-		node->data = data; \
+		node->data = (void*)value; \
 		list->tail->next = node; \
 		list->tail = node; \
 	}else{ \
-		ListNode *node = Malloc(sizeof(ListNode)); \
+		ListNode *node = (ListNode*)Malloc(sizeof(ListNode)); \
+		node->data = (void*)value; \
 		list->head = list->tail = node; \
 		node->pre = node->next = 0; \
 	}
@@ -54,13 +59,28 @@ typedef struct List
 	}
 
 #define LIST_REMOVE(list, node) \
-	if(list->head){ \
-		node->next ? 0 : list->tail = node->pre; \
-		node->pre ? (node->pre->next = node->next) : list->head = list->tail = 0; \
-		list->release_handler ? list->release_handler(node->data) : 0; \
-		Free(node); \
-	}
+	if (node->next) \
+	{ \
+	list->tail = (ListNode*)node->pre; \
+	} \
+	if (node->pre) \
+	{ \
+		node->pre->next = node->next; \
+	}else{\
+		list->head = list->tail = 0; \
+	}\
+	if (list->release_handler){ \
+			list->release_handler(node->data); \
+	} \
+	Free(node); \
 
+
+#define LIST_TRAVERSE(list, handler) \
+	ListNode *tmp = list->head; \
+	while(tmp){ \
+		handler(tmp->data); \
+		tmp = tmp->next; \
+	}
 
 
 #endif // _list_h_
