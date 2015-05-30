a7aa3d06470022a20ce4c205fa27d44f2eb12795
cokeboL <165128167@qq.com>
2015-05-30 10:28:46 +0800

    -

--- server/src/list.h
+++ server/src/list.h
@@ -1,112 +1,184 @@
 #ifndef _list_h_
 #define _list_h_
 
-#if 0
 #include <stdlib.h>
+#ifndef Malloc
 #define Malloc malloc
+#endif
+#ifndef Free
 #define Free free
 #endif
 
-#include "commen.h"
+/*
+#define LIST_CREATE()
+#define LIST_INIT(list, handler)
+#define LIST_HEAD(list) (list->head)
+#define LIST_TAIL(list) (list->tail)
+#define LIST_PUSH(list, value)
+#define LIST_REMOVE(list, node)
+#define LIST_REMOVE_IF(list, rm_if)
+#define LIST_TRAVERSE(list, handler)
+#define LIST_TRAVERSE2(list, handler)
+#define LIST_DESTROY(list)
+*/
 
-typedef void (*ListNodeReleaseHandler)(void *);
-typedef void (*ListNodeTraverseHandler)(void *);
-
+typedef void(*ListNodeHandler)(void *);
 
 typedef struct ListNode
 {
-	struct ListNode *pre;
-	struct ListNode *next;
-	void *data;
+struct ListNode *pre;
+struct ListNode *next;
+void *data;
 }ListNode;
 
 typedef struct List
 {
-	ListNode *head;
-	ListNode *tail;
-	int count;
-	ListNodeReleaseHandler release_handler;
+ListNode *head;
+ListNode *tail;
+int count;
+ListNodeHandler release_handler;
 }List;
 
-#define LIST_CREATE() \
-(List*)Malloc(sizeof(List))
+#define LIST_CREATE() (List*)Malloc(sizeof(List))
 
-#define LIST_INIT(list, handler) \
-if (list){ \
+#define LIST_INIT(list, handler)\
+if (list)\
+{\
 list->head = 0; \
 list->tail = 0; \
 list->count = 0; \
-list->release_handler = (ListNodeReleaseHandler)handler; \
+list->release_handler = (ListNodeHandler)handler; \
 }
 
-#define LIST_PUSH(list, value) \
-do{ \
-if(list->head){ \
+#define LIST_HEAD(list) (list->head)
+
+#define LIST_TAIL(list) (list->tail)
+
+#define LIST_PUSH(list, value)\
+do\
+{\
+if (list->head)\
+{\
 ListNode *node = (ListNode*)Malloc(sizeof(ListNode)); \
 node->pre = list->tail; \
 node->next = 0; \
 node->data = (void*)value; \
 list->tail->next = node; \
 list->tail = node; \
-}else{ \
+}\
+else\
+{\
 ListNode *node = (ListNode*)Malloc(sizeof(ListNode)); \
 node->data = (void*)value; \
 list->head = list->tail = node; \
 node->pre = node->next = 0; \
-} \
-}while (0);
+}\
+} while (0)
 
 
-#define LIST_POP(list) \
-do{\
-if (list->head){\
+#define LIST_POP(list)\
+do\
+{\
+if (list->head)\
+{\
 ListNode *tmp = list->tail; \
-if (list->tail->pre){\
+if (list->tail->pre)\
+{\
 list->tail = list->tail->pre; \
 list->tail->next = 0; \
-} \
+}\
+else\
+{\
+list->head = list->head = 0;\
+}\
 list->release_handler ? list->release_handler(tmp->data) : 0; \
 Free(tmp); \
-} \
-} while (0);
-
-#define LIST_REMOVE(list, node) \
-do{\
-if (node->pre) \
-{ \
-node->pre->next = node->next; \
-if (node->next) \
-{ \
-node->next->pre = (ListNode*)node->pre; \
-} \
-else{\
-list->tail = (ListNode*)node->pre; \
-} \
-}else{\
-list->head = list->tail = 0; \
 }\
-if (list->release_handler){\
-list->release_handler(node->data); \
-} \
-Free(node); \
-} while (0);
+} while (0)
 
-#define LIST_TRAVERSE(list, handler) \
-do{\
+#define LIST_REMOVE(list, node)\
+do\
+{\
+ListNode *tmp = node; \
+if (tmp->pre)\
+{\
+tmp->pre->next = tmp->next; \
+if (tmp->next)\
+{\
+tmp->next->pre = tmp->pre; \
+}\
+else\
+{\
+list->tail = tmp->pre; \
+}\
+}\
+else\
+{\
+list->head = tmp->next; \
+if (tmp->next)\
+{\
+tmp->next->pre = 0; \
+}\
+else\
+{\
+list->tail = 0; \
+}\
+}\
+if (list->release_handler)\
+{\
+list->release_handler(tmp->data); \
+}\
+Free(tmp); \
+} while (0)
+
+#define LIST_REMOVE_IF(list, rm_if)\
+do\
+{\
+ListNode **node = &list->head, *entry; \
+while (*node)\
+{\
+entry = *node; \
+if (rm_if(entry->data))\
+{\
+*node = entry->next; \
+LIST_REMOVE(list, entry); \
+}\
+else\
+{\
+node = &entry->next; \
+}\
+}\
+} while (0)
+
+#define LIST_TRAVERSE(list, handler)\
+do\
+{\
 ListNode *tmp = list->head; \
-while (tmp){ \
+while (tmp)\
+{\
 handler(tmp->data); \
 tmp = tmp->next; \
-} \
-} while (0);
+}\
+} while (0)
 
-#define LIST_TRAVERSE2(list, handler) \
-do{\
+#define LIST_TRAVERSE2(list, handler)\
+do\
+{\
 ListNode *tmp = list->tail; \
-while (tmp){\
+while (tmp)\
+{\
 handler(tmp->data); \
 tmp = tmp->pre; \
-} \
-} while (0);
+}\
+} while (0)
+
+#define LIST_DESTROY(list)\
+do\
+{\
+while (list->head)\
+{\
+LIST_POP(list); \
+}\
+} while (0)
 
 #endif // _list_h_
