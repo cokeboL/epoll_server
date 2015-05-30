85426c38d566fe209198c3b2e9671a573a210cda
cokeboL <165128167@qq.com>
2015-05-30 13:23:18 +0800

    -

--- server/src/testList.c
+++ server/src/testList.c
@@ -4,26 +4,69 @@
 /*
 #define LIST_CREATE()
 #define LIST_INIT(list, release_handler)
-#define LIST_PUSH(list, data) 
-#define LIST_POP(list) 
+#define LIST_PUSH(list, data)
+#define LIST_POP(list)
 #define LIST_REMOVE(list, node)
 */
+
+void release(void *data)
+{
+	printf("%d\n", (int)data);
+}
 
 void print(void *data)
 {
 	printf("%d\n", (int)data);
 }
 
+int rm(void *data)
+{
+	if ((int)data % 2)
+	{
+		return 1;
+	}
+	return 0;
+}
 int main()
 {
 	List *list = LIST_CREATE();
-	LIST_INIT(list, 0);
+	LIST_INIT(list, release);
 
-	int i=0;
-	for(i=0; i<10; i++)
+	ListNode *p = 0;
+	int i = 0;
+	for (i = 1; i<11; i++)
 	{
 		LIST_PUSH(list, i);
+		if (i == 3)
+		{
+			p = list->tail;
+		}
 	}
-
+	
 	LIST_TRAVERSE(list, print);
+	printf("**********\n");
+	LIST_REMOVE_IF(list, rm);
+	LIST_TRAVERSE(list, print);
+	printf("**********\n");
+	printf("head: %d\n", (int)list->head->data);
+	printf("head: %p %p\n", list->head->pre, list->head->next);
+	LIST_REMOVE(list, list->head);
+	printf("tail: %d\n", (int)list->tail->data);
+	printf("tail: %p %p\n", list->tail->pre, list->tail->next);
+	printf("**********\n");
+	LIST_TRAVERSE(list, print);
+	printf("**********\n");
+	for (i = 1; i<11; i++)
+	{
+		LIST_PUSH(list, i);
+		if (i == 3)
+		{
+			p = list->tail;
+		}
+	}
+	LIST_TRAVERSE(list, print);
+	LIST_DESTROY(list);
+	printf("**********\n");
+	LIST_TRAVERSE(list, print);
+	getchar();
 }
