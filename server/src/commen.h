f46295327bbaf05da17c05b0b9502c61eef2efd4
cokeboL <165128167@qq.com>
2015-06-01 23:27:57 +0800

    Update commen.h
    -

--- server/src/commen.h
+++ server/src/commen.h
@@ -15,7 +15,7 @@
 #define SERVER_NUM 2    //监听事件的server线程数
 #define HANDLER_NUM 3   //处理io事件及逻辑的handler线程数
 
-#define MAX_FD_NUM      0xFFFFF   //最大fd值
+#define MAX_FD_NUM      0x1FFFF   //最大fd值
 #define MAX_CLIENT_NUM  0xFFFF    //最大连接数
 #define MAX_MSG_LEN     (1024*12) //最大消息长度
 
